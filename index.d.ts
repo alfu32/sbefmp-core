2a60928f1eec82b64459c995c7337541efe8b79a
Ioan ALferaru <ioan.alferaru@gmail.com>
2019-05-26 08:42:54 +0200

    Export from dist

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export { AppTitle, AppSidebar, AppToolbar, AppContent, AppStatusbar, AppLayout } from './components/app-layout.component';
-export { TabTitle, Tab, Tabs } from './components/tab-layout.component';
-export { RXCanvas } from './components/rx-canvas.component';
-export { Marble, SvgTimeline } from './components/svg-timeline';
-export { IndexedList, IndexedListTitle, IndexedListStatus } from './components/indexed-list.component';
+// export { AppTitle, AppSidebar, AppToolbar, AppContent, AppStatusbar, AppLayout } from './components/app-layout.component';
+// export { TabTitle, Tab, Tabs } from './components/tab-layout.component';
+// export { RXCanvas } from './components/rx-canvas.component';
+// export { Marble, SvgTimeline } from './components/svg-timeline';
+// export { IndexedList, IndexedListTitle, IndexedListStatus } from './components/indexed-list.component';
